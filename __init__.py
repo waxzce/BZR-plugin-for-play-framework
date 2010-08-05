4c002dcc363c3643c2fa7cc52a5bc30643a3e1b3
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-08-05 16:42:41 +0200

    cosmetic issues in shell

--- __init__.py
+++ __init__.py
@@ -11,11 +11,12 @@ version_info = (0, 1, 0, 0)
 
 def play_post_pull_hook(pull_result):
 	if os.path.exists("framework/src/play"):
-		print "launch the play ant script"
+		print "Buildind Play! framework"
 		savedPath = os.getcwd()
 		os.chdir('framework')
-		subprocess.Popen('ant')
+		subprocess.Popen('ant').wait()
 		os.chdir(savedPath)
+		print "Done building Play! framework"
 
-branch.Branch.hooks.install_named_hook('post_pull', play_post_pull_hook, 'Play post_pull hook')
+branch.Branch.hooks.install_named_hook('post_pull', play_post_pull_hook, 'Play! post_pull hook')
 
