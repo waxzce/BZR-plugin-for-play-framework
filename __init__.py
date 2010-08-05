9a0912f0618a73b84fe32b20bc108475b6e9ca23
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-08-05 16:29:00 +0200

    add ant logic

--- __init__.py
+++ __init__.py
@@ -1,18 +1,21 @@
-## play build bzr plugin
+#!/usr/bin/env python
+# play build bzr plugin
 
-"""A plugin for launch the play ant script after pull
-"""
-import subprocess
-import os.path
+""A plugin to launch the play ant script after pull
+""
 
 from bzrlib import branch
+import os,os.path,subprocess
 
-version_info = (0, 1, 0, 'dev', 0)
+version_info = (0, 1, 0, 0)
 
 def play_post_pull_hook(pull_result):
 	if os.path.exists("framework/src/play"):
 		print "launch the play ant script"
-		#LAUNCH ANT HERE
+		savedPath = os.getcwd()
+		os.chdir('framework')
+		subprocess.Popen('ant')
+		os.chdir(savedPath)
 
-branch.Branch.hooks.install_named_hook('post_pull', play_post_pull_hook, 'play post_pull hook')
+branch.Branch.hooks.install_named_hook('post_pull', play_post_pull_hook, 'Play post_pull hook')
 
