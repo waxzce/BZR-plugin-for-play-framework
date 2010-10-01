2b260ba7e661496f8246ec6985104972a8cc64be
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-10-01 22:21:46 +0200

    wait end of pull before building

--- __init__.py
+++ __init__.py
@@ -5,18 +5,18 @@
 """
 
 from bzrlib import branch
-import os,os.path,subprocess
+import os,os.path
 
-version_info = (0, 1, 0, 0)
+version_info = (0, 2, 0, 0)
 
 def play_post_pull_hook(pull_result):
 	if os.path.exists("framework/src/play"):
-		print "Building Play! framework"
 		savedPath = os.getcwd()
 		os.chdir('framework')
-		subprocess.Popen('ant').wait()
-		os.chdir(savedPath)
+		print "Building Play! framework"
+		os.execl('/usr/bin/ant')
 		print "Done building Play! framework"
+		os.chdir(savedPath)
 
 branch.Branch.hooks.install_named_hook('post_pull', play_post_pull_hook, 'Play! post_pull hook')
 
